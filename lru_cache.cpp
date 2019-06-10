f75ed9bbab4ab8dde9aebc4926e2083a5702517c
cuiliqing <cuiliqing@gmail.com>
2019-06-10 19:51:59 +0800

    new

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -1,75 +1,119 @@
-struct CacheNode{
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+struct Node
+{
     int key;
     int val;
-    CacheNode *pre, *next;
-    CacheNode(int k, int val): key(k), val(val), pre(NULL), next(NULL);
+    Node* pre;
+    Node* next;
+    Node(int k, int v):key(k), val(v), pre(NULL), next(NULL);
 };
 
-// 设置容量（还可以设置失效时间）
-LRUCache(int capacity){
-    size = capacity;
-    head = NULL;
-    tail = NULL;
-}
+class LRUCache{
 
-// 节点删除操作
-void remove(CacaheNode* node){
-    if(node->pre != NULL){
-        node->pre->next = node->next;
-    }else {
-        head = next->next;
-    }
-    if (node -> next != NULL){
-        node-> next->pre = node->pre;
-    }else{
-        tail = node-> pre;
-    }
-}
-
-// insert
-void setHead(CacheNode* node){
-    node->next = head;
-    node->pre = NULL;
+private:
+    int count;
+    int size ;
+    map<int,Node *> mp;
+    Node *cacheHead;
+    Node *cacheTail;
     
-    if (head != NULL){
-        head->pre = node;
+public:
+    LRUCache(int capacity) {
+        size = capacity;
+        cacheHead = NULL;
+        cacheTail = NULL;
+        count = 0;
     }
-    head = node;
-    if(tail == NULL){
-        tail = head;
+    
+    int get(int k) {
+        if (cacheHead == NULL) {
+            return -1;
+        }
+        map<int, Node*>::iterator it = mp.find(k);
+        if (it == mp.end()) {
+            return -1;
+        } esle {
+            Node* p = it->second;
+            pushFront(p);
+        }
+        return cacheHead->value;
     }
-}
-
-//get
-int get(int key){
-    map<int, CacheNode*>::iterator iter = mp.find(key);
-    if (iter != mp.end()){
-        CacheNode* node = it->second;
-        remove(node);
-        setHead(node);
-        return node->val;
-    }else{
-        return -1;
+    
+    void set(intk, int v) {
+        if (cacheHead == NULL) {
+            cacheHead = (Node*) malloc(sizeof(Node));
+            cacheHead->key = k;
+            cacheHead->value = v;
+            cacheHead->pre = NULL;
+            cacheHead->next = NULL;
+            mp[key] = cacheHead;
+            cacheTail = cacheHead;
+            count++;
+        } else {
+             map<int,Node *>::iterator it=mp.find(key);
+             if (it == mp.end()) {
+                 if (count == size) {
+                     if (cacheHead==cacheTail&&cacheHead!=NULL) { //只有一个节点
+                        mp.erase(cacheHead->key);
+                        cacheHead->key = key;
+                        cacheHead->value = value;
+                        mp[key] = cacheHead;
+                     } else {
+                         Node * p =cacheTail;
+                         cacheTail->pre->next = cacheTail->next;  
+                         cacheTail = cacheTail->pre;
+                         mp.erase(p->key);
+                         p->key= key;
+                         p->value = value;
+                         p->next = cacheHead;
+                         p->pre = cacheHead->pre;
+                         cacheHead->pre = p;
+                         cacheHead = p;
+                         mp[cacheHead->key] = cacheHead;
+                     }
+                 } else {
+                      Node * p = (Node *)malloc(sizeof(Node));
+                      p->key = key;
+                      p->value = value;
+                     
+                      p->next = cacheHead;
+                      p->pre = NULL;
+                      cacheHead->pre = p;
+                      cacheHead = p;
+                      mp[cacheHead->key] = cacheHead;
+                      count++;
+                 }
+             } else {
+                 Node *p = it->second;   
+                 p->value = value;
+                 pushFront(p);
+             }
+        }
     }
-}
-
-void set(int key, int val){
-    map<int, CacheNode*>::iterator it = mp.find(key);
-    if (it != mp.end()){
-        CacheNode* node = it-> second;
-        node->val = val;
-        remove(node);
-        setHead(node);
-    }else{
-        CacheNode* newNode = new CacheNode(key, val);
-        if (mp.size() > size){
-            map<int, CacheNode*>:: iterator it = mp.find(tail->key);
-            remove(tail);
-            mp.earse(iter);
-       }
-       setHead(newNode);
-       mp[key] = newNode;
-
+    void pushFront(Node* cur) {
+        //双向链表删除节点,并将节点移动链表头部，O(1)
+        if (count == 1) {
+            return;
+        }
+        if (cur == cacheHead) {
+            return;
+        }
+        if (cur == cacheTail) {
+            cacheTail = cur->pre;
+        }
+        
+        cur->pre->next = cur->next;
+        if (cur->next != NULL) {
+            cur->next->pre = cur->pre;
+        }
+        cur->next = cacheHead;
+        cur->pre = NULL;
+        cacheHead->pre = cur;
+        cacheHead = cur;
     }
 }
 
